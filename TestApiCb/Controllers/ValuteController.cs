d4b1c9a79100b9e2990a07fb86f0eb0adb698c26
Evgeny Lifanov <lifanov.j@yandex.ru>
2023-01-27 15:47:24 +0300

    Добавление контроллеров

---
+++ TestApiCb/Controllers/ValuteController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestApiCb.Interfaces;
+using TestApiCb.Models;
+
+namespace TestApiCb.Controllers
+{
+        [Route("currency")]
+        [ApiController]
+        public class ValuteController : ControllerBase
+        {
+            private readonly ICurrencyRepositiry _currencyRepositiry;
+            public ValuteController(ICurrencyRepositiry currencyRepositiry)
+            {
+                _currencyRepositiry = currencyRepositiry;
+            }
+
+            [HttpGet("{CharCod}")]
+            public async Task<ActionResult<Valutes>> GetValuteBuIdAsync(string CharCod)
+            {
+                try
+                {
+                    var result = await _currencyRepositiry.GetValuteAsync(CharCod);
+                    return Ok(result);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+    
+}
---
+++ TestApiCb/Controllers/ValutesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestApiCb.Interfaces;
+using TestApiCb.Models;
+
+namespace TestApiCb.Controllers
+{
+    [Route("currencies")]
+    [ApiController]
+    public class ValutesController : ControllerBase
+    {
+        private readonly ICurrencyRepositiry _currencyRepositiry;
+        public ValutesController(ICurrencyRepositiry currencyRepositiry)
+        {
+            _currencyRepositiry = currencyRepositiry;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Valutes>>> GetValuteAllAsync(int? pageNum = null, int? pageSize = null)
+        {
+            try
+            {
+                var result = await _currencyRepositiry.GetValutesAllAsync();
+                if (pageNum == null && pageSize == null)
+                {
+                    return Ok(result);
+                }
+
+                return Ok(result.GroupBy(x => x.Name)
+                                .Skip((pageNum ?? 0) * pageSize ?? 5)
+                                .Take(pageSize ?? 5));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+
+}
