f835a8ba22e14a6b822a5452e6f16c8dc53c5355
Evgeny Lifanov <lifanov.j@yandex.ru>
2023-01-27 15:21:02 +0300

    Добавление моделей

---
+++ TestApiCb/Models/Root.cs
@@ -0,0 +1,11 @@
+﻿namespace TestApiCb.Models
+{
+    public class Root
+    {
+        public string? Date { get; set; }
+        public string? PreviousDate { get; set; }
+        public string? PreviousURL { get; set; }
+        public string? Timestamp { get; set; }
+        public Dictionary<string, Valutes>? Valute { get; set; }
+    }
+}
---
+++ TestApiCb/Models/Valutes.cs
@@ -0,0 +1,13 @@
+﻿namespace TestApiCb.Models
+{
+    public class Valutes
+    {
+        public string? ID { get; set; }
+        public string? NumCode { get; set; }
+        public string? CharCode { get; set; }
+        public int Nominal { get; set; }
+        public string? Name { get; set; }
+        public decimal Value { get; set; }
+        public decimal Previous { get; set; }
+    }
+}
